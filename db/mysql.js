37083e3edc983d1e4421f1ec2efc4bd2c3386948
ovidionavarro <ovidionavarro90@gmail.com>
2024-02-10 21:48:09 -0500

    arreglando colecciones

--- db/mysql.js
+++ db/mysql.js
@@ -1,5 +1,5 @@
 const {Sequelize}= require('sequelize')
- const db=new Sequelize('parking_db',process.env.DATABASE_USER,process.env.DATABASE_PASS,{
+ const db=new Sequelize('parking_db1',process.env.DATABASE_USER,process.env.DATABASE_PASS,{
     dialect:'mysql',
     host:'localhost',
     logging:false
